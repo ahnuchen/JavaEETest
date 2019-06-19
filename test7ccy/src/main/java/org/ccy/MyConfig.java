1d8367595370e5085b4725a2f30010ad2994c397
chenchangyu003 <chenchangyu003@huazhu.com>
2019-06-19 19:19:28 +0800

    Scope

---
+++ test7ccy/src/main/java/org/ccy/MyConfig.java
@@ -0,0 +1,12 @@
+package org.ccy;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("org.ccy")
+public class MyConfig {
+
+}
---
+++ test7ccy/src/main/java/org/ccy/ScopeTest.java
@@ -0,0 +1,9 @@
+package org.ccy;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class ScopeTest {
+}
